ecbbcef68369bfa712aa4a5623e97cb83bb2529e
nslc0000 <345407647@qq.com>
2017-07-03 15:23:40 +0800

    zhpc

--- dream/zhihupc/js/controller.js
+++ dream/zhihupc/js/controller.js
@@ -1,20 +1,7 @@
-var ctrl=angular.module('ctrl',[]);
-ctrl.controller('index',function($ser,$scope){
-     $ser.getwheel.then(function(data){
-     	    $scope.listdata=data.stories;
-     	    var data=data.stories;
-     	    var datas=data.splice(3,4);
-            $scope.data=datas;
-     });
-});
-ctrl.controller('list',function($ser,$scope,$routeParams){
-              $ser.getId($routeParams.id).then(function(data){
-                   $scope.data=data;
-              })
-});
-ctrl.controller('shownews',function($ser,$scope,$routeParams){
-    console.log($routeParams)
-    $ser.getshowId($routeParams.id).then(function(data){
-        $scope.body=data;
-    })
-});
+// var ctrl=angular.module('ctrl',['service']);
+// ctrl.controller('ctrl1',function($scope,$service){
+//          $service.then(function(data){
+//               $scope.data=data;
+//                console.log(data)
+//          })
+// })
--- dream/zhihupc/js/index.js
+++ dream/zhihupc/js/index.js
@@ -1,49 +1,31 @@
-window.onload=function () {
-    var navdj=document.querySelector(".navdj");
-    var nav=document.querySelector(".nav");
-    var syall=document.querySelector(".sy_all");
-     var set=document.querySelector(".set");
-     var syselect=document.querySelector(".sy_select");
-     var day=document.querySelector("#day");
-     var navlist=document.querySelectorAll(".navlist>ul");
-     var indexp=document.querySelector(".indexp");
-    navdj.addEventListener("touchend",function () {
-        nav.style.left="0";
-        nav.style.transition="all 0.5s";
-        syall.style.display="block";
-    });
-     syall.addEventListener("touchend",function () {
-        nav.style.left="-6.35rem";
-        nav.style.transition="all 0.5s";
-        syall.style.display="none";
-        syselect.style.display="none";
-    });
-      set.addEventListener("touchend",function () {
-          syselect.style.display="block";
-          syall.style.display="block";
-    });
-    navlist.forEach(function(v){
-        v.addEventListener("touchend",function () {
-            nav.style.left="-6.35rem";
-            nav.style.transition="all 0.5s";
-            syall.style.display="none";
-        });
+var app=angular.module('app',['ngRoute','service','dir','ngSanitize']);
+app.config(function($routeProvider){
+    $routeProvider.when('/list/:id',{
+        templateUrl:'tpl/list.html',
+         controller:function($scope,$name,$routeParams){
+             $name.getId($routeParams.id).then(function(listdata){
+                 $scope.listdata=listdata;
+             })
+         }
+    }).when('/',{
+        templateUrl:'tpl/list_one.html',
+        controller:function($scope,$name){
+            $name.getnew().then(function(newdata){
+                $scope.newdata=newdata.stories;
+            })
+        }
+    }).when('/show/:id',{
+        templateUrl:'tpl/show.html',
+        controller:function($scope,$name,$routeParams){
+            $name.getshow($routeParams.id).then(function(show){
+                $scope.show=show;
+            })
+            
+        }
     })
-    indexp.addEventListener("touchend",function () {
-        nav.style.left="-6.35rem";
-        nav.style.transition="all 0.5s";
-        syall.style.display="none";
-    });
-      function includeLinkStyle(url) {
- var link = document.createElement("link");
- link.rel = "stylesheet";
- link.type = "text/css";
- link.href = url;
- document.getElementsByTagName("head")[0].appendChild(link);
-}
-  day.addEventListener("touchend",function () {
-          includeLinkStyle("css/night.css");
-          syselect.style.display="none";
-
-    });
-};
+});
+app.run(function($rootScope,$name){
+    $name.getall.then(function(data){
+        $rootScope.data=data.others
+    })
+})
